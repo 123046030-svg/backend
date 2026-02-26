c13ba96738242ad64225d9a44281c1238e1eb11d
123046030@upq.edu.mx <123046030@upq.edu.mx>
2026-02-26 11:24:33 -0600

    asd

--- core/settings.py
+++ core/settings.py
@@ -5,17 +5,18 @@ BASE_DIR = Path(__file__).resolve().parents[1]  # .../app
 
 class Settings(BaseSettings):
     # SMTP
-    MAIL_USERNAME: str
-    MAIL_PASSWORD: str
-    MAIL_FROM: str
+    MAIL_USERNAME: str = "sduop1@geq.net"
+    MAIL_PASSWORD: str = "1234567"
+    MAIL_FROM: str = "sduop1@geq.net"
     MAIL_FROM_NAME: str = "SDUOP"
-    MAIL_SERVER: str
+    MAIL_SERVER: str = "am11313a.geq.net"
     MAIL_PORT: int = 587
 
     MAIL_STARTTLS: bool = True
     MAIL_SSL_TLS: bool = False
     USE_CREDENTIALS: bool = True
     VALIDATE_CERTS: bool = True
+    MAIL_TEST_RECIPIENT: str = "scop@queretaro.gob.mx"
 
     # Templates
     TEMPLATE_FOLDER: Path = BASE_DIR / "templates"
